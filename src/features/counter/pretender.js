17fc9b92f815f741e9f4478d2a896752e34f6089
knoxknox <alexandr_y@hotmail.com>
2023-01-31 12:38:02 +0200

    Fix linter warnings

--- src/features/counter/pretender.js
+++ src/features/counter/pretender.js
@@ -6,9 +6,9 @@ const incrementMock = ({ counter }) => JSON.stringify(mock.increment(counter));
 const decrementMock = ({ counter }) => JSON.stringify(mock.decrement(counter));
 
 export function increment() {
-  this.get('/increment', (request) => [200, headers, incrementMock(request.queryParams)], 500);
+  this.get('/increment', request => [200, headers, incrementMock(request.queryParams)], 500);
 }
 
 export function decrement() {
-  this.get('/decrement', (request) => [200, headers, decrementMock(request.queryParams)], 500);
+  this.get('/decrement', request => [200, headers, decrementMock(request.queryParams)], 500);
 }
