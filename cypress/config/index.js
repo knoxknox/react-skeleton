c85d33843be3e62eec40db4f11e4f13b0a91ef48
knoxknox <alexandr_y@hotmail.com>
2023-01-31 18:52:02 +0200

    Configure cypress

---
+++ cypress/config/index.js
@@ -0,0 +1,6 @@
+// Processed and loaded automatically before your test files.
+// This is a great place to put global configuration that modifies Cypress.
+
+// Example:
+// Cypress.Commands.add('login', (email, password) => { ... });
+// Cypress.Commands.overwrite('visit', (fn, url, options) => { ... });
